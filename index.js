9f2736cc00bb44ca60d7068bc3224a8add69fb76
Pratit Shivaji Todkar <Pratittodkar@gmail.com>
2023-11-07 01:22:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const crypto = require('crypto')
-const cors = require('cors')
-const KJUR = require('jsrsasign')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
 
-const app = express()
-const port = process.env.PORT || 4000
+const app = express();
+const port = process.env.PORT || 4000;
 
-app.use(bodyParser.json(), cors())
-app.options('*', cors())
+app.use(bodyParser.json(), cors());
+app.options('*', cors());
 
-app.post('/', (req, res) => {
+// New endpoint for triggering the Zoom OAuth token acquisition
+app.post('/getToken', (req, res) => {
+  const zoomOAuthTokenUrl = 'https://zoom.us/oauth/token';
+  const clientId = process.env.ZOOM_CLIENT_ID; // Get the client ID from environment variable
+  const clientSecret = process.env.ZOOM_CLIENT_SECRET; // Get the client secret from environment variable
+  const requestBody = {
+    code: req.body.authorizationCode, // Get the authorization code from the request body
+    grant_type: 'authorization_code',
+    redirect_uri: req.body.redirectUri, // Get the redirect URI from the request body
+    code_verifier: req.body.codeVerifier, // Get the code verifier from the request body
+  };
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+  };
 
-  const iat = Math.round(new Date().getTime() / 1000) - 30;
-  const exp = iat + 60 * 60 * 2
+  axios
+    .post(zoomOAuthTokenUrl, requestBody, { headers })
+    .then((response) => {
+      console.log('Response from Zoom OAuth token endpoint:', response.data);
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error('Error obtaining Zoom OAuth token:', error);
+      res.status(500).json({ error: 'Failed to obtain OAuth token' });
+    });
+});
 
-  const oHeader = { alg: 'HS256', typ: 'JWT' }
-
-  const oPayload = {
-    app_key: process.env.ZOOM_VIDEO_SDK_KEY,
-    tpc: req.body.sessionName,
-    role_type: req.body.role,
-    user_identity: req.body.userIdentity,
-    session_key: req.body.sessionKey,
-    geo_regions: req.body.geoRegions,
-    cloud_recording_option: req.body.cloudRecordingOption,
-    cloud_recording_election: req.body.cloudRecordingElection,
-    version: 1,
-    iat: iat,
-    exp: exp
-  }
-
-  const sHeader = JSON.stringify(oHeader)
-  const sPayload = JSON.stringify(oPayload)
-  const signature = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, process.env.ZOOM_VIDEO_SDK_SECRET)
-
-  res.json({
-    signature: signature
-  })
-})
-
-app.listen(port, () => console.log(`Zoom Video SDK Auth Endpoint Sample Node.js listening on port ${port}!`))
+app.listen(port, () =>
+  console.log(`Zoom Video SDK Auth Endpoint Sample Node.js listening on port ${port}!`)
+);
